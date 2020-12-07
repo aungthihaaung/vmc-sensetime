99d871132aa0a872669609b1d4432e2a5d7b2d8a
Aung Thiha Aung <aungthiha@gmail.com>
2020-12-07 12:56:32 +0800

    fix merge issue

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -2,7 +2,6 @@ import { ScanVisitorResult } from "lib/types";
 import React, { useState } from "react";
 
 export const getServerSideProps = ({ query, res }) => {
-<<<<<<< HEAD
 	return { props: { status: query.status, timeout: query.my_timeout } };
 };
 
@@ -98,91 +97,6 @@ export default function Home({ status, timeout }) {
 		titleStyleClass = "title-error";
 		message = "YOU SHALL NOT PASS!!!";
 	}
-=======
-  return { props: { status: query.status, timeout: query.my_timeout } };
-};
-
-export default function Home({ status, timeout }) {
-  const [showLoading, setShowLoading] = useState(false);
-
-  let leftSidePanelImgPath = "";
-  let leftSidePanelImgMaxWidth = 0;
-  let title = "";
-  let message = "";
-  let message2 = "";
-  let titleStyleClass = "";
-  let messageStyleClass = "message"; // default is message
-
-  setTimeout(() => {
-    setShowLoading(true);
-  }, timeout);
-
-  if (showLoading) {
-    return (
-      <>
-        <div
-          className="cc-logo-big"
-          style={{ textAlign: "center", fontWeight: "bold" }}
-        >
-          <img
-            src={
-              // "https://recledmi.sirv.com/Images/vmc_sense_time/centricore.png?w=130&h=52"
-              "/images/centricore_big.webp"
-            }
-            alt="centricore"
-          />
-          <span style={{ paddingLeft: 23 }}>Processing...</span>
-        </div>
-      </>
-    );
-  }
-
-  if (status === ScanVisitorResult.OK) {
-    leftSidePanelImgPath =
-      // "https://recledmi.sirv.com/Images/vmc_sense_time/man.gif?w=150&h=250";
-      "/images/man.webp";
-    leftSidePanelImgMaxWidth = 150;
-    title = "THANK YOU";
-    titleStyleClass = "title-ok";
-    messageStyleClass = "message-ok";
-    message = "Please Proceed to Enter";
-    message2 = "请继续输入。";
-  } else if (status === ScanVisitorResult.STAFF_OK) {
-    leftSidePanelImgPath =
-      // "https://recledmi.sirv.com/Images/vmc_sense_time/man.gif?w=150&h=250";
-      "/images/man.webp";
-    leftSidePanelImgMaxWidth = 150;
-    title = "STAFF";
-    titleStyleClass = "title-ok";
-    messageStyleClass = "message-ok";
-    message = "Please Proceed to Enter";
-    message2 = "请继续输入。";
-  } else if (status === ScanVisitorResult.QUOTA_FULL) {
-    leftSidePanelImgPath =
-      // "https://recledmi.sirv.com/Images/vmc_sense_time/stop_sign.png?w=150&h=250";
-      "/images/stop_sign.webp";
-    leftSidePanelImgMaxWidth = 150;
-    title = "QUOTA FULL";
-    titleStyleClass = "title-error";
-    message = "Entry quota is exceeded.";
-  } else if (status === ScanVisitorResult.NOT_FOUND) {
-    leftSidePanelImgPath =
-      // "https://recledmi.sirv.com/Images/vmc_sense_time/stop_sign.png?w=150&h=250";
-      "/images/stop_sign.webp";
-    leftSidePanelImgMaxWidth = 150;
-    title = "WARNING";
-    titleStyleClass = "title-error";
-    message = "No record found.";
-  } else if (status === ScanVisitorResult.BLACKLIST) {
-    leftSidePanelImgPath =
-      // "https://recledmi.sirv.com/Images/vmc_sense_time/stop_sign.png?w=150&h=250";
-      "/images/stop_sign.webp";
-    leftSidePanelImgMaxWidth = 150;
-    title = "ACCESS DENIED";
-    titleStyleClass = "title-error";
-    message = "YOU SHALL NOT PASS!!!";
-  }
->>>>>>> stg
 
 	const leftPanel = (
 		<div style={{ marginTop: 0 }}>
@@ -198,7 +112,6 @@ export default function Home({ status, timeout }) {
 		</div>
 	);
 
-<<<<<<< HEAD
 	return (
 		<div className="main-layout">
 			<div className="cc-logo">
@@ -216,25 +129,6 @@ export default function Home({ status, timeout }) {
 			<div className={titleStyleClass}>
 				<div className="title-text">{title}</div>
 			</div>
-=======
-  return (
-    <div className="main-layout">
-      <div className="cc-logo">
-        <img
-          src={
-            // "https://recledmi.sirv.com/Images/vmc_sense_time/centricore.png?w=130&h=52"
-            "/images/centricore.webp"
-          }
-          alt="centricore"
-        />
-      </div>
-      <div>
-        <div style={{ height: 20 }}>&nbsp;</div>
-      </div>
-      <div className={titleStyleClass}>
-        <div className="title-text">{title}</div>
-      </div>
->>>>>>> stg
 
 			<div style={{ height: "100%", border: "0px solid red" }}>
 				<table style={{ borderWidth: 0, height: "100%", width: "97%" }}>
@@ -246,7 +140,6 @@ export default function Home({ status, timeout }) {
 									<div className="parallelogram1"></div>
 									<div className="parallelogram2"></div>
 
-<<<<<<< HEAD
 									<div className="text-box">
 										<table
 											cellPadding="10"
@@ -276,29 +169,6 @@ export default function Home({ status, timeout }) {
 													</td>
 												</tr>
 												{/* <tr>
-=======
-                  <div className="text-box">
-                    <table
-                      cellPadding="10"
-                      style={{ borderWidth: 0, height: "100%" }}
-                    >
-                      <tbody>
-                        <tr>
-                          <td
-                            style={{ verticalAlign: "middle", height: "50%" }}
-                            className={messageStyleClass}
-                          >
-                            <div>
-                              {/* {message1 &&
-                                typeof message1 === "string" &&
-                                Parser(message1)} */}
-                              {message}
-                            </div>
-                            {message2.length > 0 && <div>{message2}</div>}
-                          </td>
-                        </tr>
-                        {/* <tr>
->>>>>>> stg
                     <td>B</td>
                   </tr> */}
 											</tbody>
