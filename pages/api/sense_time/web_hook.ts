505613d8f1574e7ea690c28e6f4421f82fd8ffdf
centricore <admin@centricore.co>
2021-02-11 12:27:37 +0800

    demo set changes

--- pages/api/sense_time/web_hook.ts
+++ pages/api/sense_time/web_hook.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { myRedis } from "lib/misc/redis";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	myRedis.pub(`SENSE_TIME.MESSAGE_IN`, req.body);
-	res.status(200).json({ whatever: "whatever" });
+  myRedis.pub(`SENSE_TIME.MESSAGE_IN`, req.body);
+  console.log(req.body);
+  console.log("****");
+  res.status(200).json({ text: "whatever" });
 };
