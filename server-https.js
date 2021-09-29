d8744c0ad41fad3c8bb55967fc9d14eae61b9557
Aung Thiha Aung <aungthiha@gmail.com>
2021-09-29 12:24:12 +0800

    change https to http server

--- server-https.js
+++ server-https.js
@@ -1,6 +1,7 @@
-const { createServer } = require("http");
+const { createServer } = require("https");
 const { parse } = require("url");
 const next = require("next");
+const fs = require("fs");
 
 const dev = false; // process.env.NODE_ENV !== 'production';
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
@@ -8,12 +9,17 @@ console.log("dev", dev);
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const httpsOptions = {
+  key: fs.readFileSync("./certs/client-key.pem"),
+  cert: fs.readFileSync("./certs/client-cert.pem"),
+};
+
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(8787, (err) => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:8787");
+    console.log("> Ready on https://localhost:8787");
   });
 });
